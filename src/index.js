8eb99affc28a6f7d53e78975ceb3657bd667b5df
unknown <robertgaamess@gmail.com>
2022-05-13 15:09:17 -0300

    loop  do loop

--- src/index.js
+++ src/index.js
@@ -10,3 +10,11 @@ while (c <= 10) {
   console.log(c);
   c++;
 }
+var c = 1;
+while (c <= 10) {
+  var modulo = c % 2;
+  if (modulo == 0) {
+    console.log(c);
+  }
+  c++;
+}
--- src/index2.js
+++ src/index2.js
@@ -1,8 +1,15 @@
-var c = 1;
-while (c <= 10) {
-  var modulo = c % 2;
-  if (modulo == 0) {
-    console.log(c);
+let text = "11";
+let invertido = text.split(" ");
+console.log(invertido);
+invertido.reverse().join(" ");
+
+function tome(a1, a2) {
+  let Array = []
+  for (let i = a1; i < a2; i++) {
+    if (i.toString()== i.toString().split('').reverse().join('') {
+      Array.push
+    }
+
   }
-  c++;
+  console.log(Array);
 }
