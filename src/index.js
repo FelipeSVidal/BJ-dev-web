f205ac2dd437646537ca324e5d40ad835aa71752
unknown <robertgaamess@gmail.com>
2022-05-26 15:09:39 -0300

    começo

--- src/index.js
+++ src/index.js
@@ -18,3 +18,20 @@ while (c <= 10) {
   }
   c++;
 }
+
+let text = "11";
+let invertido = text.split(" ");
+console.log(invertido);
+invertido.reverse().join(" ");
+
+function tome(a1, a2) {
+  let Array = [];
+  for (let i = a1; i < a2; i++) {
+    if (i.toString() == i.toString().split("").reverse().join("")) {
+      Array.push(i);
+    }
+  }
+  console.log(Array);
+}
+
+tome(1, 100);
