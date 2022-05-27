a06689f3dc368207fdfe9d8451108e77c9e77863
unknown <raquelccosta00@gmail.com>
2022-05-27 15:01:03 -0300

    paradise

---
+++ src/index.js
@@ -0,0 +1,12 @@
+var number = 1;
+while(number <=10)}
+var module=number  %2
+if(mosulw == 0)}
+console.log(number)
+}
+number++;
+}
+for(var number =1; number <= 10; number++)}
+var module=number %2
+if (module ==0)}
+console.log(number)
