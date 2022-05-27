d209a66accb905141f2aff568feb2b55a9f46f9e
unknown <lucassantos20170@gmail.com>
2022-05-27 15:00:55 -0300

    Pety

---
+++ src/java.js
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<h2>JavaScript Strings</h2>
+
+<p id="demo"></p>
+
+<script>
+let text = "Pedro Silva Santos";  // String written inside quotes
+document.getElementById("demo").innerHTML = text;
+</script>
+
+</body>
+</html>
