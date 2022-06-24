d85f77a7708b68dbe11a1944922e235e1a85ddc8
unknown <hugonapoliaoh@gmail.com>
2022-06-24 15:07:44 -0300

    hugo

--- desafio.js
+++
@@ -1,7 +0,0 @@
-function desafio(a1, a2) {
-  for (let i = a1; i <= a2; i++) {
-    if (i.toString() == i.toString().split("").reverse().join("")) {
-        
-    }
-  }
-}
---
+++ src/desafio.js
@@ -0,0 +1,31 @@
+function desafio(a1, a2) {
+  for (let i = a1; i <= a2; i++) {
+    if (i.toString() == i.toString().split("").reverse().join("")) {
+    }
+  }
+}
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(async (resposta) => {
+    const usuarios = await resposta.json();
+    usuarios.forEach((element) => {
+      const elemento = document.createElement("tr");
+      const id = document.createElement("td");
+      id.innerHTML = element.id;
+      const nome = document.createElement("td");
+      nome.innerHTML = element.name;
+      const email = document.createElement("td");
+      email.innerHTML = element.email;
+      const empresa = document.createElement("td");
+      empresa.innerHTML = element.company.name;
+      elemento.appendChild(id);
+      elemento.appendChild(nome);
+      elemento.appendChild(email);
+      elemento.appendChild(empresa);
+      document.getElementById("quadro").appendChild(elemento);
+    });
+  })
+  .catch((erro) => {
+    alert("Deu erro");
+    console.log(erro);
+  });
