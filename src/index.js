53c2de212f6e24d9822ac75b37930ab0d9117ee9
unknown <raquelccosta00@gmail.com>
2022-06-24 14:37:07 -0300

    atuali

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-var number = 1;
-while(number <= 10)}
-var module=number  %2
-if(mosulw == 0)}
-console.log(number)
-}
-number++;
-}
-for(var number =1; number <= 10; number++)}
-var module=number %2
-if (module == 0)}
-console.log(number)
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(async (resposta) => {
+    const usuarios = await resposta.json();
+    usuarios.forEach(element => {
+      const elemento = document.createElement("tr");
+      const id = document.createElement("td");
+      id.innerHTML = element.id;
+      const nome = document.createElement("td");
+      nome.innerHTML = element.name;
+      const email = document.createElement("td");
+      email.innerHTML = element.email;
+      const empresa = document.createElement("td");
+      empresa.innerHTML = element.company.name;
+      elemento.appendChild(id);
+      elemento.appendChild(nome);
+      elemento.appendChild(email);
+      elemento.appendChild(empresa);
+      document.getElementById("quadro").appendChild(elemento);  
+    });
+})
+.catch((erro) => {
+    alert ("Deu erro");
+    console.log(erro);
+});
